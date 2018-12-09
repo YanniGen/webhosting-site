f658f5dca8f1825a3c5c8925671e918af5ac2467
YanniGen <yguenov@gmail.com>
2018-12-08 23:08:43 -0500

    Adding Main content component with example cards for services styling header some more

--- server-site/src/app/app.module.ts
+++ server-site/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './shared/material.module';
 import { HeaderComponent } from './components/header/header.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule,
